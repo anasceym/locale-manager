dc63c021d962fca6c2988da014931ac94a64290d
anasceym <anasfirdauss@gmail.com>
2016-10-16 20:02:37 +0800

    WIP

--- tests/api/ExportApiTest.php
+++ tests/api/ExportApiTest.php
@@ -6,6 +6,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExportApiTest extends TestCase
 {
+
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -21,7 +24,35 @@ class ExportApiTest extends TestCase
      */
     public function it_should_able_to_export_to_file_for_all_namespaces() {
 
-        $this->json('get', '/api/projects/1/export/file');
+        $testData = collect([
+            'namespace' => 'export_test',
+            'lang_codes' => ['en', 'ms', 'kln']
+        ]);
+
+        // Initialize all need data
+
+        $project = factory(App\Project::class)->create();
+
+        $namespace = factory(App\Project_namespace::class)->create(['name' => $testData->get('namespace'),'project_id' => $project->id]);
+
+        $langs = collect([]);
+
+        foreach($testData->get('lang_codes') as $code) {
+
+            $langs->push(factory(App\Project_lang::class)->create(['lang_code' => $code, 'project_id' => $project->id]));
+        }
+
+        $testTranslationKeys = collect([
+            'welcome_text',
+            'bye_text',
+            'welcome_paragraph'
+        ]);
+
+        // Make the request
+
+        $this->json('get', "/api/projects/{$project->id}/export/file");
+
+        // Assert!
 
         $this->assertResponseStatus(200);
     }
