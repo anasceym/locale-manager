b2e75cbfd0f9006a1b3c820aa0d9de54e5adcdc2
anasceym <anasfirdauss@gmail.com>
2016-11-13 16:16:25 +0800

    Removed unnecessary use

--- tests/api/ExportTest.php
+++ tests/api/ExportTest.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
-use Storage;
 
 class ExportTest extends TestCase
 {
